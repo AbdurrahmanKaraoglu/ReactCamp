316cb8a2072f9072ab1cb970031e7c374a520db4
AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>
2021-06-09 22:38:57 +0300

    9 Haziran 2021 22:38
    Signed-off-by: AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CartSummary from './CartSummary'
-import {Container, Menu } from 'semantic-ui-react'
+import { Container, Menu } from 'semantic-ui-react'
 import SignedOut from './SignedOut'
 import SignedIn from './SignedIn'
 
 export default function Navi() {
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+    function handleSignOut(params) {
+        setIsAuthenticated(false)
+    }
+
+    function handleSignIn(params) {
+        setIsAuthenticated(true)
+    }
     return (
         <div>
             <Menu inverted fixed="top">
                 <Container>
-                    <Menu.Item name='home'/>
-                    <Menu.Item name='messages'/>
+                    <Menu.Item name='home' />
+                    <Menu.Item name='messages' />
                     <Menu.Menu position='right' >
-                        <CartSummary>
+                        <CartSummary />
+                        {isAuthenticated ? <SignedIn signOut={handleSignOut} bisey="1" /> : <SignedOut signIn={handleSignIn} giris="2" />}
 
-                        </CartSummary>
-                        <SignedOut/>
-                        <SignedIn/>
                     </Menu.Menu>
                 </Container>
 
--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Menu,Dropdown,Image } from 'semantic-ui-react'
 
-export default function SignedIn() {
+export default function SignedIn(props) {
     return (
         <div>
             <Menu.Item>
@@ -9,7 +9,7 @@ export default function SignedIn() {
                 <Dropdown pointing >
                     <Dropdown.Menu>
                         <Dropdown.Item text = "Bilgilerim" icon="info" />
-                        <Dropdown.Item text = "Çıkış Yap" icon="sign-out" />
+                        <Dropdown.Item onClick={props.signOut} text = "Çıkış Yap" icon="sign-out" />
                         
                     </Dropdown.Menu>
                 </Dropdown>
