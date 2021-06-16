13f406c3ad95a1c979fb2e9c252998d62d6471d4
AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>
2021-06-16 21:49:46 +0300

    camp-react
    Signed-off-by: AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,32 +1,36 @@
-import React, { useState } from 'react'
-import CartSummary from './CartSummary'
-import { Container, Menu } from 'semantic-ui-react'
-import SignedOut from './SignedOut'
-import SignedIn from './SignedIn'
+import React, {useState} from "react";
+import CartSummary from "./CartSummary";
+import { Container, Menu } from "semantic-ui-react";
+import SignedOut from "./SignedOut";
+import SignedIn from "./SignedIn";
+import { useHistory } from "react-router";
 
 export default function Navi() {
-    const [isAuthenticated, setIsAuthenticated] = useState(true)
-    function handleSignOut(params) {
-        setIsAuthenticated(false)
-    }
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const history = useHistory()
+  function handleSignOut() {
+    setIsAuthenticated(false)
+    history.push("/")
+  }
 
-    function handleSignIn(params) {
-        setIsAuthenticated(true)
-    }
-    return (
-        <div>
-            <Menu inverted fixed="top">
-                <Container>
-                    <Menu.Item name='home' />
-                    <Menu.Item name='messages' />
-                    <Menu.Menu position='right' >
-                        <CartSummary />
-                        {isAuthenticated ? <SignedIn signOut={handleSignOut} bisey="1" /> : <SignedOut signIn={handleSignIn} giris="2" />}
+  function handleSignIn() {
+    setIsAuthenticated(true)
+  }
 
-                    </Menu.Menu>
-                </Container>
+  return (
+    <div>
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
 
-            </Menu>
-        </div>
-    )
+          <Menu.Menu position="right">
+            <CartSummary/>
+            {isAuthenticated?<SignedIn signOut={handleSignOut} bisey="1"/>
+            :<SignedOut signIn={handleSignIn}/>}  
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
 }
--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Menu,Dropdown,Image } from 'semantic-ui-react'
+import { Dropdown, Menu,Image } from 'semantic-ui-react'
 
-export default function SignedIn(props) {
+export default function SignedIn({signOut}) {
     return (
         <div>
             <Menu.Item>
-                <Image avatar spaced="right" src="https://react.semantic-ui.com/logo.png"></Image>
-                <Dropdown pointing >
+                <Image avatar spaced="right" src="https://avatars.githubusercontent.com/u/74272647?v=4"/>
+                <Dropdown pointing="top left" text="Abdurrahman">
                     <Dropdown.Menu>
-                        <Dropdown.Item text = "Bilgilerim" icon="info" />
-                        <Dropdown.Item onClick={props.signOut} text = "Çıkış Yap" icon="sign-out" />
-                        
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
                     </Dropdown.Menu>
                 </Dropdown>
             </Menu.Item>
         </div>
     )
 }
+
+//22.50 Dersteyiz
